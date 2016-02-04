5ff91116658d2a5288bd07aacb483fd692be253b
Wang <walden.wang@torfcbdmx1341.na.corp.ipgnetwork.com>
2016-02-04 10:37:06 -0500

    added copy to clipboard function

--- Assets/global/js/global.js
+++ Assets/global/js/global.js
@@ -1,1 +1,14 @@
-<!-- empty file -->
+// copy to clipboard funcyion
+var copyTextareaBtn = document.querySelector('.textareacopybtn');
+
+copyTextareaBtn.addEventListener('click', function(event) {
+  var copyTextarea = document.querySelector('.copytextarea');
+  copyTextarea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    
+  } catch (err) {
+    alert("Your browser does not support this function,\nplease select and copy manually instead.");
+  }
+});
