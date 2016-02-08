d01cf49f86073a2b1170b40b52aed99434216aad
hollygood <kathykehuang@gmail.com>
2016-02-08 17:40:53 -0500

    customize navigation

--- assets/global/js/global.js
+++ assets/global/js/global.js
@@ -14,8 +14,31 @@ copyTextareaBtn.addEventListener('click', function(event) {
 });
 
 $("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
 });
 
-$("#sidebar-wrapper").load("../assets/global/header.html");
+$("#sidebar-wrapper").load("../assets/global/header.html", function(){	
+
+	$(".sidebar-nav li").on('click', function(){
+	
+		if ($(this).find('ul').length != 0) {
+			$(this).toggleClass('active');
+
+			if ($(this).hasClass('active')) {
+				$(this).find('.nav-second-level').first().stop(true, true).slideDown();
+				$('.arrow', this).toggleClass('fa-angle-down fa-angle-left');
+			} else {
+				$(this).find('.nav-second-level').first().stop(true, true).slideUp();
+				$('.arrow', this).toggleClass('fa-angle-left fa-angle-down');
+			}
+
+		} else {
+			$(".sidebar-nav li").removeClass('active');
+			$(this).addClass('active');
+		}
+	});
+
+
+});
+
