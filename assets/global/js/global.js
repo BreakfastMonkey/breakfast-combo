44da4f5a5bbd7e53a9d9bf37b7b5338f47873f16
Walden Wang <waldenwangyayi@gmail.com>
2016-02-09 17:18:00 -0500

    lots of fixes

--- assets/global/js/global.js
+++ assets/global/js/global.js
@@ -1,24 +1,6 @@
 // copy to clipboard funcyion
-var copyTextareaBtn = document.querySelector('.textareacopybtn');
 
-copyTextareaBtn.addEventListener('click', function(event) {
-  var copyTextarea = document.querySelector('.copytextarea');
-  copyTextarea.select();
-
-  try {
-    var successful = document.execCommand('copy');
-    
-  } catch (err) {
-    alert("Your browser does not support this function,\nplease select and copy manually instead.");
-  }
-});
-
-$("#menu-toggle").click(function(e) {
-  e.preventDefault();
-  $("#wrapper").toggleClass("toggled");
-});
-
-$("#sidebar-wrapper").load("../assets/global/header.html", function(){	
+$("#dynamicHeader").load("../assets/global/header.html", function(){	
 
 	$(".sidebar-nav li").on('click', function(){
 
@@ -41,6 +23,10 @@ $("#sidebar-wrapper").load("../assets/global/header.html", function(){
 		}
 	});
 
+	$("#menu-toggle").click(function(e) {
+	  e.preventDefault();
+	  $("#wrapper").toggleClass("toggled");
+	});
 
 });
 
--- assets/tools/js/url_decoder_encoder.js
+++ assets/tools/js/url_decoder_encoder.js
@@ -35,3 +35,17 @@ $("#encoder").click(function(){
 $("#clear").click(function(){
   rmAvtive();
 });
+
+var copyTextareaBtn = document.querySelector('.textareacopybtn');
+
+  copyTextareaBtn.addEventListener('click', function(event) {
+    var copyTextarea = document.querySelector('.copytextarea');
+    copyTextarea.select();
+
+    try {
+      var successful = document.execCommand('copy');
+      
+    } catch (err) {
+      alert("Your browser does not support this function,\nplease select and copy manually instead.");
+    }
+  });
