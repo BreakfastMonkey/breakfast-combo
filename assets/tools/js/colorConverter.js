691a20a5ae037fa842f2eb0ac037d77d8b74725b
luluMily <x2011bhb@gmail.com>
2016-02-08 23:43:24 -0500

    rgb to hex

--- assets/tools/js/colorConverter.js
+++ assets/tools/js/colorConverter.js
@@ -1,12 +1,11 @@
-
 /*JavaScript only for colorConverter*/
 var app = angular.module('colorConverterTool', []);
 
 app.controller('colorConverterCtrl', function($scope) {
+    var result = '';
     $scope.newColor = '#ffffff';
 
     $scope.toRgb = function() {
-        var result;
         if( $scope.hexValue !== null ) {
             var hex = $scope.hexValue.replace('#','');
             if ( hex.length === 3 ) {
@@ -14,15 +13,23 @@ app.controller('colorConverterCtrl', function($scope) {
             }
         }
 
-        if( hex.length >= 3 ) {
-            var r = parseInt(hex.substring(0,2), 16);
-            var g = parseInt(hex.substring(2,4), 16);
-            var b = parseInt(hex.substring(4,6), 16);
-
-            result = 'rgb('+r+','+g+','+b+')';
-        }
+        result = (hex.length == 3 || hex.length > 4) ? "rgb(" +
+          parseInt(hex.substring(0,2), 16) + "," +
+          parseInt(hex.substring(2,4), 16) + "," +
+          parseInt(hex.substring(4,6), 16) + ")" : '';
 
         $scope.rgbValue = result;
         $scope.newColor = result;
+    }
+
+    $scope.toHex = function() {
+         var rgb = $scope.rgbValue.match(/[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+         result = (rgb && rgb.length === 4) ? "#" +
+          ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+          ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+          ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+
+          $scope.hexValue = result;
+          $scope.newColor = result;
     }
 });
